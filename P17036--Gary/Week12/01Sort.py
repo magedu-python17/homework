912e49e554e5fa11083bd8a5b25c4f5709641c40
molson265 <44839357+molson265@users.noreply.github.com>
2018-12-03 20:51:20 +0800

    修改

--- P17036--Gary/Week12/01Sort.py
+++ P17036--Gary/Week12/01Sort.py
@@ -8,10 +8,17 @@ __mtime__ = '2018/12/3'
     I love animals. They taste delicious.        
 """
 
-# 冒泡排序
+# 冒泡排序   升序
 lst =[98,2,4,78,12,1,6,3,10,34,55]
 for i in range(len(lst)):
-    for j in range(i):
+    for j in range(len(lst)-1):
         if lst[j]>lst[j+1]:
+            lst[j],lst[j+1] = lst[j+1],lst[j]
+print(lst)
+
+# 冒泡排序   降序
+for i in range(len(lst)):
+    for j in range(len(lst)-1):
+        if lst[j]<lst[j+1]:
             lst[j],lst[j+1] = lst[j+1],lst[j]
 print(lst)
