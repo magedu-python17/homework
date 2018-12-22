2e17a5275e45f3547092dd6dca40b4c6650e1750
gdgoodman <19412452@qq.com>
2018-12-22 11:00:27 -0500

    square

---
+++ P17084_阿孟/a视频中的作业/chapter_02/square2.py
@@ -0,0 +1,10 @@
+#*******************************************
+# welcome to my blog : http://101.132.77.52
+#*******************************************
+
+#print square
+s=int(input("num:"))
+for i in range(s):  #Count.Record how many times you enter.
+	for i in range(s):
+		print("*",end='')
+	print()
