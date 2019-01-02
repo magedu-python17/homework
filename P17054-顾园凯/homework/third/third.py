888772f797ebfb336d713c8a3e6d5e9c7c7c3a1f
molson265 <44839357+molson265@users.noreply.github.com>
2019-01-03 02:08:48 +0800

    Update third.py
    n = input('请输入一串只包含大小写字母和数字的字符串')
    lowercase = [i for i in n if i.islower()]
    capital = [i for i in n if i.isupper()]
    oddnumber = [i for i in n if i.isdigit() and int(i) % 2 == 1]
    evennumber = [i for i in n if i.isdigit() and int(i) % 2 == 0]
    lowercase.sort()
    capital.sort()
    oddnumber.sort()
    evennumber.sort()
    print(''.join(lowercase) + ''.join(capital) + ''.join(oddnumber) + ''.join(evennumber))

--- P17054-顾园凯/homework/third/third.py
+++ P17054-顾园凯/homework/third/third.py
@@ -4,3 +4,5 @@ capital = [i for i in n if i.isupper()]
 oddnumber = [i for i in n if i.isdigit() and int(i) % 2 == 1]
 evennumber = [i for i in n if i.isdigit() and int(i) % 2 == 0]
 print(''.join(lowercase) + ''.join(capital) + ''.join(oddnumber) + ''.join(evennumber))
+
+#代码写的蛮不错的，把排序的功能实现就非常完美
