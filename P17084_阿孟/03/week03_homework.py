9e3fdf8b23f9a038684118880c5531f28679c551
gdgoodman <19412452@qq.com>
2019-01-14 21:25:32 +0800

    第三周作业提交

---
+++ P17084_阿孟/03/week03_homework.py
@@ -0,0 +1,42 @@
+#========================================================
+#
+# Author: AM readygood@163.com
+#
+# Blog: http://www.my-blog.top
+#
+# Last modified: 2019-01-14 21:16
+#
+# Filename: week03_homework.py
+#
+# Description: V1.0
+#
+#========================================================
+#
+#第三次的作业：
+#  s = "Sorting1234" 
+#给定一个只包含大小写字母，数字的字符串，对其进行排序，保证：
+#1 所有的小写字母在大写字母前面
+#2 所有的字母在数字前面
+#3 所有的奇数在偶数前面 
+#=========================
+#还未学到函数，所以用比较笨的方式完成
+while 1:
+    lst = str(input('pls input num or letter:'))
+    oddlst = []
+    evenlst = []
+    letter = []
+    if lst == 'stop':
+        break
+    for i in lst:
+        if i <= '9':
+            number = i
+            if not int(number) % 2:
+                evenlst.append(number)
+            else:
+                oddlst.append(number)
+        else:
+            letter.append(i)
+        evenlst.sort()
+        oddlst.sort()
+        letter.sort(reverse=True)
+    print(''.join(letter + oddlst + evenlst))
