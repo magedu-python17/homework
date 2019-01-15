248997153aab229dca658cfadac30ac1740a98e8
chengduzhoujing <854021135@qq.com>
2019-01-15 22:04:33 +0800

    第四次作业

---
+++ P17081-zhoujing/homework4/homework4.py
@@ -0,0 +1,11 @@
+comments= (
+    "Implementation note",
+    "Changed",
+    "ABC for generator"
+)
+
+def add_ellipsis(lstinfo,length=10,dic={}):
+	dic=dict(zip(lstinfo,[info[:length]+'.......' if len(info)>length else info for info in lstinfo]))
+	return '\n'.join(dic.values())
+print(add_ellipsis(comments,length=7))
+    
