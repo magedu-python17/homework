d857520acbbd5f74026bfe548a8a1d8f19f47987
lqmoonxiaobaibai <3148489341@qq.com>
2019-01-20 10:05:34 +0800

    第四次作业提交

---
+++ P17014-天津-刘强/liuqiang-fourthzuoye.py
@@ -0,0 +1,31 @@
+comments = [
+    "Implementation note",
+    "Changed",
+    "ABC for generator",
+]
+def func(comments,max = 6):
+    lst = []
+    for string  in  comments:
+        if len(string) > max:
+            lst.append(string[:max] + '...')
+        else:
+            lst.append(string)
+    return  lst
+print("\n".join(func(comments)))
+
+
+def add(comments, max_length):
+    for comment in comments:
+        comment = comment.strip()
+        if len(comment) > max_length:
+            yield comment[:max_length] + '...'
+        else:
+            yield comment
+comments = (
+    "Implementation note",
+    "Changed",
+    "ABC for generator",
+)
+x = add(comments,7)
+for i in x:
+    print(i)
