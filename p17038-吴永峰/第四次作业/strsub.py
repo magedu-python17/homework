b507e78ca67a300caa74f1f517f540544daad74f
wyf677678 <44830841+wyf677678@users.noreply.github.com>
2019-01-26 10:06:24 +0800

    更新

--- p17038-吴永峰/第四次作业/strsub.py
+++ p17038-吴永峰/第四次作业/strsub.py
@@ -5,13 +5,15 @@ pattern = "......"
 
 def add_ellipsis(n: int, string):
     for i in range(len(string)):
-        regex = re.compile(string[i][n:])
-        newstr = regex.sub(pattern, string[i])
-        string[i] = newstr
+        if len(string[i]) >= n:
+            regex = re.compile(string[i][n:])
+            newstr = regex.sub(pattern, string[i])
+            string[i] = newstr
+
     print(string)
 
 
-add_ellipsis(4, comments)
+add_ellipsis(10, comments)
 
 # 元组的话是不是可以转成列表后操作，不知道理解的对不对
 # comments = ('lmplementation note', 'changed', 'ABC for generator')
@@ -21,10 +23,11 @@ add_ellipsis(4, comments)
 # def add_ellipsis(n: int, string):
 #     comment = list(string)
 #     for i in range(len(comment)):
-#         regex = re.compile(comment[i][n:])
-#         newstr = regex.sub(pattern, comment[i])
-#         comment[i] = newstr
+#         if len(comment[i]) >= n:
+#             regex = re.compile(comment[i][n:])
+#             newstr = regex.sub(pattern, comment[i])
+#             comment[i] = newstr
 #     print(comment)
-#
+
 #
 # add_ellipsis(4, comments)
