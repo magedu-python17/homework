258998361709888c31c7dc86c484566637eda8e1
EvanNing <ningmq@outlook.com>
2019-02-21 23:00:09 +0800

    add week05-1

---
+++ P17075-上海-宁明强/homework/week05-1.py
@@ -0,0 +1,43 @@
+
+from collections import Iterable
+def fn1(x):
+    return x*x
+
+def fn2(x, y):
+    return x + y
+
+def fn3(n):
+    return n%2==1
+def mymap(fn,iter1):
+    if not isinstance(iter1,Iterable):
+        return 'Iterable is not Iterable'
+    else:
+        lst=[]
+        for i in iter1:
+            lst.append(fn(i))
+    return lst
+
+def myreduce(fn,iter1):
+    if not isinstance(iter1,Iterable):
+        return 'Iterable is not Iterable'
+    else:
+        lst=0
+        for i in iter1:
+            lst=fn(lst,i)
+    return lst
+
+def myfilter(fn,iter1):
+    if not isinstance(iter1,Iterable):
+        return 'Iterable is not Iterable'
+    else:
+        lst=[]
+        for i in iter1:
+            if fn(i):
+                lst.append(i)
+    return lst
+
+print(mymap(fn1,(2,3,3,4,5,6)),'\n',mymap(fn1,123))
+print(myreduce(fn2,(2,3,3,4,5,6)),'\n',myreduce(fn2,123))
+print(myfilter(fn3,(2,3,3,4,5,6)),'\n',myfilter(fn3,123))
+
+
--- P17075-上海-宁明强/test.py
+++ P17075-上海-宁明强/test.py
@@ -1,10 +1,17 @@
-#接受一个参数n,n为正整数，左右两种打印方式，要求数字对齐
-def f3(x):
-    print(x)
-    weight=(10*len(str(x)) )*2+x
-    print(weight)
-    for i in range(1,x+1):
-#        print('{:>{}}'.format(' '.join(str(j) for j in range(i,0,-1)),weight))
-        print('{:>{}}'.format(' '.join(sorted((str(j) for j in range(1,x-i)),reverse=True,key=int)),weight))
+from collections import Iterable
+def fn1(n):
+    return n % 2 == 1
 
-f3(22)
+def resemblefilter(fn,args):
+    if isinstance(args, Iterable):
+        lst = []
+        for i in args:
+            if fn(i):
+                lst.append(i)
+#                print(lst)
+        return lst
+    else:
+        print("args is not iterable!")
+
+print(tuple(resemblefilter(fn1,[2,3,3,4,5,6])))
+#print(tuple(resemblefilter(lambda x:x%2==1 ,[2,3,3,4,5,6])))
