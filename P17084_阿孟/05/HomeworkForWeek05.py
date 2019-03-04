3fcb5f74a64741f2aa71ef1885b3b8a5e5408bc7
gdgoodman <19412452@qq.com>
2019-03-04 23:28:56 +0800

    week05.

--- P17084_阿孟/05/HomeworkForWeek05.py
+++ P17084_阿孟/05/HomeworkForWeek05.py
@@ -24,3 +24,18 @@ def mapself(fn,iterable):
 print(mapself(fn,[1,2,3,4,5]))
 lst = list(mapself(fn,[1,2,3,4,5]))
 print(lst)
+################
+def fn(x):
+    return x % 2 == 1
+
+def filterself(fn,iterable):
+    lst = []
+    if not isinstance(iterable,(dict,list,tuple,set)):
+        print("ERROR")
+    else:
+        for i in iterable:
+            if fn(i) == True:
+                lst.append(i)
+    return lst
+
+print(filterself(fn,[1,2,3,4,5,6,7,8,9]))
