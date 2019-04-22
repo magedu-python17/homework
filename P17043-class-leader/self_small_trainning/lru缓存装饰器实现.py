ddb43142f36c7c3690042282e06acb3485f7e362
eveyrdayx <everydayx@163.com>
2019-04-22 11:47:17 +0800

    lru 缓存装饰器实现

--- P17043-class-leader/self_small_trainning/lru缓存装饰器实现.py
+++ P17043-class-leader/self_small_trainning/lru缓存装饰器实现.py
@@ -1,9 +1,16 @@
-import hashlib
 import inspect
 
+only_dict = dict()
 
 def cache(fn):
     def wrapper(*args,**kwargs):
+        '''
+        本来用hash MD5实现
+        现在根据老师讲的inspect模块实现的进阶版本
+        :param args:
+        :param kwargs:
+        :return:
+        '''
         # m = hashlib.md5()
         # arg = '{}'.format(sorted(list(args) + [v for k, v in kwargs.items()]))
         # print(arg)
@@ -15,21 +22,20 @@ def cache(fn):
         #     return ret
         # else:
         #     return hash_dict[md5]
+        only_tuple = tuple()
+        global only_dict
         sig = inspect.signature(fn)
         params = sig.parameters
         keys = params.keys()
         values = params.values()
-        only_dict = dict()
-        only_tuple = tuple()
         for i in zip(keys,args):
             only_tuple += i
         for k, v in sorted(kwargs.items()):
-            only_tuple += (k,v)
+            only_tuple += only_tuple + (k,v)
         for k , v in sorted(params.items()):
             if v.default is not inspect._empty :
                 if k not in only_tuple:
                     only_tuple += (k,v.default)
-
         if only_tuple in only_dict.keys():
             return only_dict[only_tuple]
         else:
@@ -39,15 +45,12 @@ def cache(fn):
     return wrapper
 
 
-#@cache
-# def fib(n):
-#     if n in (1,2):
-#         return 1
-#     return fib(n-2) + fib(n-1)
-#
-#
-# print([fib(i+1) for i in range(35)])
-# print(hash_dict)
+@cache
+def fib(n):
+    if n in (1,2):
+        return 1
+    return fib(n-2) + fib(n-1)
+
 
 @cache
 def add(x,y=2,z=10):
@@ -61,4 +64,4 @@ print(add(z=3,y=2,x=1))
 print(add(y=2,x=1,z=3))
 print(add(1,y=2,z=3))
 print(add(x=1,y=2,z=3))
-
+print([fib(i+1) for i in range(35)])
